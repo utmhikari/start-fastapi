60db9ca629dfd928efc6406d2028b8f7a94fe7e9
utmhikari <361914599@qq.com>
2020-03-06 21:33:50 +0800

    add example of uploading multipart-form file

--- controller/base.py
+++ controller/base.py
@@ -1,9 +1,26 @@
 from application.controller import success
-from fastapi import APIRouter
+from application.logger import get_controller_logger
+from fastapi import APIRouter, UploadFile, File
 
 router = APIRouter()
+
+LOGGER = get_controller_logger('BASE')
 
 
 @router.get('/')
 def get_root():
     return success({'Hello': 'World'})
+
+
+@router.post('/upload')
+async def upload_file(file: UploadFile = File(...)):
+    filename = file.filename
+    content = await file.read()
+    LOGGER.info('Received file %s:\n%s' % (
+        filename,
+        content.decode('utf-8')
+    ))
+    return success({
+        'filename': filename,
+        'contentType': file.content_type
+    })
